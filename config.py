1c87ef92a94f89527eb79c68d06007d2f9239555
Alireza <77078513+alirezatae@users.noreply.github.com>
2025-12-12 12:48:46 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
+ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set")
+
+if not ADMIN_CHAT_ID:
+    raise RuntimeError("ADMIN_CHAT_ID is not set")
+
+ADMIN_CHAT_ID = int(ADMIN_CHAT_ID)
