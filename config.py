8e1f2659a66354c85593580f5b976da71590cee4
Alireza <77078513+alirezatae@users.noreply.github.com>
2025-12-12 12:46:04 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = os.environ["BOT_TOKEN"]
-ADMIN_IDS = [
-    int(x.strip())
-    for x in os.environ.get("ADMIN_IDS", "").split(",")
-    if x.strip()
-]
+load_dotenv()
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
